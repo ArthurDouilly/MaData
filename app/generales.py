6bfe15289735477f3c0b0fa25e325c0143699888
ArthurDouilly <arthurdouilly@hotmail.fr>
2026-03-16 11:15:48 +0100

    ajouts additionnel aux routes

--- app/generales.py
+++ app/generales.py
@@ -15,15 +15,18 @@ from flask import render_template
 
 @app.route("/home")
 def home():
-    return "WIP"
+    return "pages/home est WIP"
 
 # route de la page /about
 
 @app.route("/home/about")
 def about():
-    return render_template("pages/about.html")
+    return "pages/about est WIP"
+    # return render_template("pages/about.html")
 
-# --- routes liées à une catégorie de données ---
+# ----- routes liées à une catégorie de données -----
+
+# --- expositions ---
 
 # route de la page de recensement des expositions
 
@@ -38,35 +41,81 @@ def toutes_expositions():
 
 return render_template("pages/toutes_expos.html", donnees=donnees, resultats=resultats, sous_titre="Toutes les expositions du MAD")
 
-# --- expositions ---
-
 # route de la page d'une exposition en particulier
 
 @app.route("/expositions/<string:expo>")
 def exposition(expo):
-    ###
+    ### ... à réaliser
     return render_template("pages/expo.html", expo=nom) ###)
 
 # --- activités ---
 
 # route de la page de recensement des types d'activités
+@app.route("/activites")
+def toutes_activites():
+    resultats = Activites.query.all()
+    donnees = []
+    for activite in resultats:
+        donnees.append({
+            "nom": activites.types_activites
+        })
+
+return render_template("pages/toutes_activites.html", donnees=donnees, resultats=resultats, sous_titre="Toutes les activités du MAD")
 
 # route de la page d'un type d'activité en particulier
+@app.route("/activites/<string:activite>")
+def activite(activite):
+    ### ... à réaliser
+    return render_template("pages/activite.html", activite=nom)
 
 # --- publics ---
 
 # route de la page de recensement des types de publics
+@app.route("/publics")
+def tous_publics():
+    resultats = Publics.query.all()
+    donnees = []
+    for public in resultats:
+        donnees.append({
+            "nom": publics.types_public
+        })
+
+return render_template("pages/tous_publics.html", donnees=donnees, resultats=resultats, sous_titre="Tous les types de publics du MAD")
 
 # route de la page d'un type de public en particulier
+@app.route("/publics/<string:public>")
+def public(public):
+    ### ... à réaliser
+    return render_template("pages/public.html", public=nom)
 
 # --- séances ---
 
 # route de la page de recensement des séances
+@app.route("/seances")
+def toutes_seances():
+    resultats = Seances.query.all()
+    donnees = []
+    for seance in resultats:
+        donnees.append({
+            "nom": seances.nom_seance
+        })
+
+return render_template("pages/toutes_seances.html", donnees=donnees, resultats=resultats, sous_titre="Tous les séances du MAD")
 
 # route de la page d'une séance en particulier
+@app.route("/seances/<string:seance>")
+def seance(seance):
+    ### ... à réaliser
+    return render_template("pages/seance.html", seance=nom)
 
 # ----- routes liées à la recherche -----
 
 # route de la page de recherche avancée
+@app.route("/recherche_avancee")
+def recherche_avancee():
+    return "La recherche avancée est WIP"
 
 # route des résultats de la barre de recherche (pagination)
+@app.route("/recherche/")
+def résultats():
+    return "La recherche est WIP"
