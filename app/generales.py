9820f20f44b2266f9f94c6e3e23dc59dcaf2d914
ArthurDouilly <arthurdouilly@hotmail.fr>
2026-03-18 16:09:27 +0100

    encore des corrections!

--- app/generales.py
+++ app/generales.py
@@ -5,7 +5,7 @@ Fichier regroupant les routes générales, càd les routes vers les pages princi
 # ----- importation des modules python -----
 
 from ..app import app, db
-from flask import render_template
+from flask import render_template, redirect, url_for
 
 # ----- création des routes -----
 
@@ -15,15 +15,15 @@ from flask import render_template
 
 @app.route("/") # redirige immédiatement sur la page /index ci-dessous
 def home():
-    return redirect(url_for("/index")) # redirect(url_for) permet de faire la redirection
+    return redirect(url_for("index")) # redirect(url_for) permet de faire la redirection
 
-@app.route("/index") # page d'accueil du site
+@app.route("/accueil") # page d'accueil du site
 def index():
     return render_template("pages/index.html") # on utilise ici simplement un render_template
 
 # route de la page /about
 
-@app.route("/home/about")
+@app.route("/accueil/about")
 def about():
     return "pages/about est WIP"
     # return render_template("pages/about.html")
@@ -42,15 +42,15 @@ def toutes_expositions():
         donnees.append({
             "nom": expositions.nom_exposition
         })
-
-return render_template("pages/toutes_expos.html", donnees=donnees, resultats=resultats, sous_titre="Toutes les expositions du MAD")
+    return render_template("pages/expositions.html", donnees=donnees, resultats=resultats, sous_titre="Toutes les expositions du MAD")
 
 # route de la page d'une exposition en particulier
 
-@app.route("/expositions/<string:expo>")
-def exposition(expo):
-    ### ... à réaliser
-    return render_template("pages/expo.html", expo=nom) ###)
+@app.route("/expositions/<string:nom_exposition>") # route contenant le nom d'exposition en variable
+def exposition(nom_exposition):
+    return render_template("pages/une_exposition.html",
+    sous_titre=nom_exposition,
+    donnees=Expositions.query.filter(Expositions.nom_exposition == nom_exposition).first())
 
 # --- activités ---
 
@@ -63,14 +63,14 @@ def toutes_activites():
         donnees.append({
             "nom": activites.types_activites
         })
-
-return render_template("pages/toutes_activites.html", donnees=donnees, resultats=resultats, sous_titre="Toutes les activités du MAD")
+    return render_template("pages/activites.html", donnees=donnees, resultats=resultats, sous_titre="Toutes les activités du MAD")
 
 # route de la page d'un type d'activité en particulier
-@app.route("/activites/<string:activite>")
-def activite(activite):
-    ### ... à réaliser
-    return render_template("pages/activite.html", activite=nom)
+@app.route("/activites/<string:nom_activite>")
+def activite(nom_activite):
+    return render_template("pages/une_activite.html",
+    sous_titre=nom_activite,
+    donnees=Activites.query.filter(Activites.nom_activite == nom_activite).first())
 
 # --- publics ---
 
@@ -83,14 +83,14 @@ def tous_publics():
         donnees.append({
             "nom": publics.types_public
         })
-
-return render_template("pages/tous_publics.html", donnees=donnees, resultats=resultats, sous_titre="Tous les types de publics du MAD")
+    return render_template("pages/publics.html", donnees=donnees, resultats=resultats, sous_titre="Tous les types de publics du MAD")
 
 # route de la page d'un type de public en particulier
-@app.route("/publics/<string:public>")
-def public(public):
-    ### ... à réaliser
-    return render_template("pages/public.html", public=nom)
+@app.route("/publics/<string:type_public>")
+def public(type_public):
+    return render_template("pages/un_public.html",
+    sous_titre=type_public,
+    donnees=Publics.query.filter(Publics.type_public == type_public).first())
 
 # --- séances ---
 
@@ -103,23 +103,82 @@ def toutes_seances():
         donnees.append({
             "nom": seances.nom_seance
         })
-
-return render_template("pages/toutes_seances.html", donnees=donnees, resultats=resultats, sous_titre="Tous les séances du MAD")
+    return render_template("pages/seances.html", donnees=donnees, resultats=resultats, sous_titre="Tous les séances du MAD")
 
 # route de la page d'une séance en particulier
-@app.route("/seances/<string:seance>")
-def seance(seance):
-    ### ... à réaliser
-    return render_template("pages/seance.html", seance=nom)
+@app.route("/seances/<string:id_seance>")
+def seance(id_seance):
+    return render_template("pages/une_seance.html",
+    sous_titre=id_seance,
+    donnees=Seances.query.filter(Seances.id_seance == id_seance).first())
 
 # ----- routes liées à la recherche -----
 
-# route de la page de recherche avancée
-@app.route("/recherche_avancee")
-def recherche_avancee():
-    return "La recherche avancée est WIP"
+# route de la recherche rapide
+@app.route("/recherche_rapide")
+@app.route("/recherche_rapide/<int:page>")
+def recherche_rapide(page=1):
 
-# route des résultats de la barre de recherche (pagination)
-@app.route("/recherche/")
-def résultats():
-    return "La recherche est WIP"
+    ### à réaliser !
+        
+    return render_template("pages/resultats_recherche.html", 
+            sous_titre= "Recherche | " + chaine, 
+            donnees=resultats,
+            requete=chaine)
+
+# route de la page de recherche avancée
+@app.route("/recherche", methods=['GET', 'POST'])
+@app.route("/recherche/<int:page>", methods=['GET', 'POST'])
+def recherche(page=1):
+    form = Recherche() 
+
+    # initialisation des données de retour dans le cas où il n'y ait pas de requête
+    donnees = []
+
+    try:
+        if form.validate_on_submit():
+            # récupération des éventuels arguments de l'URL qui seraient le signe de l'envoi d'un formulaire
+            id_seance =  clean_arg(request.form.get("id_seance", None))
+            nom_exposition =  clean_arg(request.form.get("nom_exposition", None))
+            type_activite =  clean_arg(request.form.get("type_activite", None))
+            type_public =  clean_arg(request.form.get("type_public", None))
+
+            # si l'un des champs de recherche a une valeur, alors cela veut dire que le formulaire a été rempli et qu'il faut lancer une recherche 
+            # dans les données
+            if id_seance or nom_exposition or type_activite or type_public:
+                # initialisation de la recherche; en fonction de la présence ou nom d'un filtre côté utilisateur, nous effectuerons des filtres SQLAlchemy,
+                # ce qui signifie que nous pouvons jouer ici plusieurs filtres d'affilée
+                query_results = Seances.query
+
+                if id_seance:
+                    query_results = query_results.filter(Seances.name.ilike("%"+id_seance.lower()+"%"))
+                
+                '''
+                if nom_exposition:
+                    resource = db.session.execute("""select a.id from country a 
+                        inner join country_resources b on b.id = a.id and b.resource  == '"""+ressource+"""'
+                        """).fetchall() # à changer, il y a du dur ici !
+                    query_results = query_results.filter(Country.id.in_([r.id for r in resource] ))
+                
+                if type_activite:
+                    
+                    # chemin : Seances -> Activites
+                
+                if type_public:
+                    # chemin : Seances -> seances_publics -> Publics
+                '''
+
+                donnees = query_results.order_by(Seances.id_seance).paginate(page=page, per_page=app.config["RESULTS_PER_PAGE"]) # RESULTS_PER_PAGE à configurer !
+
+                # renvoi des filtres de recherche pour préremplissage du formulaire
+                form.nom_pays.data = nom_pays
+                form.continents.data = continent
+                form.ressources.data = ressource
+            flash("La recherche a été effectuée avec succès", "info")
+    except Exception as e:
+        flash("La recherche a rencontré une erreur "+ str(e), "info")
+
+    return render_template("pages/resultats_recherche.html", 
+            sous_titre= "Recherche" , 
+            donnees=donnees,
+            form=form)
